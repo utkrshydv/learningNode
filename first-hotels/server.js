906fcf124e47a5ce4d64f6319dfe4222f32f58d3
utkrshydv <utkarsh4socials@gmail.com>
2025-06-07 18:04:00 +0530

    learning authentication

--- first-hotels/server.js
+++ first-hotels/server.js
@@ -2,11 +2,42 @@ const express = require('express');
 const app = express();
 const db = require('./db');
 const bodyParser = require('body-parser');
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
+const Person = require('./models/Person');
 
+const logRequest = (req, res, next) => {
+  console.log(`${new Date().toLocaleString()} request made to: ${req.originalUrl}`);
+  next();
+}
 
-app.get('/', (req, res) => {
+passport.use(new LocalStrategy(async (username, password, done) => {
+
+  try{
+    console.log("Received Credentials: ", username, password)
+    const user = await Person.findOne({username: username})
+
+    if(!user){
+      return done(null, false, {message: 'incorrect username'} ); //done(error, user, info)
+    }
+
+    const isPasswordMatch = user.password == password ? true : false;
+
+    if(isPasswordMatch){
+      return done(null, user);
+    } else {
+      return done(null, false, {message: 'incorrect password'});
+    }
+  }catch(err){
+    return done(err);
+  }
+}))
+
+app.use(passport.initialize());
+
+app.get('/', logRequest, passport.authenticate('local', {session: false}) ,(req, res) => {
   res.send('Hello World')
 })
 
@@ -16,7 +47,8 @@ const personRoutes = require('./routes/personRoutes');
 app.use('/person', personRoutes);
 
 const menuRoutes = require('./routes/menuRoutes');
-app.use('/menu', menuRoutes);
+
+app.use('/menu', logRequest, menuRoutes);
 
 
 app.listen(3000, () => {
